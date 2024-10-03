864bcfacf879fdeee88402877ef26170a24b46a9
matteoaricci <matteo.a.ricci@gmail.com>
2024-10-03 15:59:34 -0400

    created basic health endpoint

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
-package jotapi
+package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Hello from the jot api!")
+	e := echo.New()
+	e.GET("/api/healthz", func(c echo.Context) error  {
+		res := struct {
+			Status string `json:"status"`
+		} {
+			Status: "OK",
+		}
+
+		return c.JSON(http.StatusOK, res)
+	})
+
+	e.Start(":8080")
 }
